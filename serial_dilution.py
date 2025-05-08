440e6761b6d0709af3c2e946116fe462956441c4
astroneeor <nerroralam@gmail.com>
2025-05-08 17:05:45 -0600

    Started Serial Dilution

---
+++ serial_dilution.py
@@ -0,0 +1,14 @@
+from opentrons import protocol_api
+
+metadata = {
+    "apiLevel": "2.16",
+    "protocolName": "Serial Dilution Tutorial Neeor",
+    "description": """Just following the tutorial of the
+                    Python API to get familiar wit the system""",
+    "author": "Neeor SURE Program"
+}
+
+requirements = {"robotType": "OT-2", "apiLevel": "2.16"}
+
+def run (protocol: protocol_api.ProtocolContext):
+    pass
