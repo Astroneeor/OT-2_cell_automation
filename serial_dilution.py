98679e0cd0f10ed3d800a8338602a37a982aab67
Astroneeor <nerroralam@gmail.com>
2025-05-09 13:26:07 -0600

    Serial dilution thing

--- serial_dilution.py
+++ serial_dilution.py
@@ -1,14 +1,27 @@
 from opentrons import protocol_api
 
 metadata = {
-    "apiLevel": "2.16",
     "protocolName": "Serial Dilution Tutorial Neeor",
     "description": """Just following the tutorial of the
                     Python API to get familiar wit the system""",
     "author": "Neeor SURE Program"
 }
 
-requirements = {"robotType": "OT-2", "apiLevel": "2.16"}
+requirements = {"robotType": "OT-2", "apiLevel": "2.22"}
 
 def run (protocol: protocol_api.ProtocolContext):
-    pass
+    # Setting Laboware items
+    tips = protocol.load_labware("opentrons_flex_96_tiprack_1000ul", "1")
+    reservoir = protocol.load_labware("nest_12_reservoir_15ml", "2")
+    plate = protocol.load_labware("nest_96_wellplate_200ul_flat", "3")
+    
+    # Pipetting Settings
+    left_pipette = protocol.load_instrument("p1000_single_gen2", "left", tip_racks=[tips])
+    
+    # Starting transfer
+    left_pipette.distribute(100, reservoir["A1"], plate.rows()[0], new_tip="once")
+
+    # Mixing solution into the plate
+    row = plate.rows()[0]
+    left_pipette.transfer(100, reservoir["A2"], row[0], mix_after=(3, 50))
+    left_pipette.transfer(100, row[:11], row[1:], mix_after=(3,50))
